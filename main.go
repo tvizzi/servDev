bbafe54b938cdc1e13a2135d1009e65fbceb59fd
tvizzi <tvizz1@mail.ru>
2024-12-19 15:51:57 +0300

    fix delete & edit csrf token

--- main.go
+++ main.go
@@ -158,7 +158,7 @@ func main() {
 
 	// CSRF Middleware
 	app.Use(csrf.New(csrf.Config{
-		KeyLookup:      "header:X-CSRF-Token",
+		KeyLookup:      "form:_csrf,header:X-CSRF-Token",
 		CookieName:     "csrf_",
 		CookieHTTPOnly: true,
 		CookieSameSite: "Strict",
